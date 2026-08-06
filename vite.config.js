4c8b1bdbe72c81e8baf1445c303e69850dedd961
Walter Christian <39551281+walterperez@users.noreply.github.com>
2026-08-06 13:24:18 +0200

    feat(git): added vite base for for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/api': 'http://localhost:3001' } },
-  build: { target: 'es2020' }
+  server: { proxy: { "/api": "http://localhost:3001" } },
+  build: { target: "es2020" },
+  base: "/Canary-Luxury-Cars/",
 });
